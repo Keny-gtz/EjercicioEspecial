bb19839aa814c6e159ba0fa1badb466d8be5fc8f
kenya <Kengut04@gmail.com>
2025-05-03 23:53:07 -0600

    Se realizo la correcion del boton loadProducts para mostrar los productos dentro de las cards

--- html/js/main.js
+++ html/js/main.js
@@ -2,8 +2,9 @@
 const mainProds = document.getElementById("mainProds");
 const loadButton = document.getElementById("loadProducts");
 const URLMain = "https://api.escuelajs.co/api/v1/products";
+const cards = document.querySelectorAll(".card");
 
-function getProducts() {
+/*function getProducts() {
   fetch(URLMain)
     .then((response) => response.json())
     .then((products) => {
@@ -16,9 +17,9 @@ function getProducts() {
           ${err.message}
         </div>`;
     });
-}
+}*/
 
-function createCards(products) {
+/*function createCards(products) {
   mainProds.innerHTML = '';
   products.forEach(product => {
     //console.log(product.title, product.images); comprobar ell contenido.
@@ -38,4 +39,54 @@ function createCards(products) {
   });
 }
 
-loadButton.addEventListener("click", getProducts);
+loadButton.addEventListener("click", getProducts);*/
+
+// Corrección de cargar la info de la Api en las tarjetas existentes.
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loadButton = document.getElementById("loadProducts");
+
+  loadButton.addEventListener("click", () => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then(response => response.json())
+      .then(productos => {
+        const cards = document.querySelectorAll(".card");
+
+        for (let i = 0; i < 9 && i < productos.length; i++) {
+          const product = productos[i];
+          const card = cards[i];
+          const existingImg = card.querySelector("img");
+          if (existingImg) {
+            existingImg.remove();
+          }
+
+          // Actualiza imagen
+          const img = card.querySelector("svg");
+          if (img) {
+            img.remove(); // Elimina SVG placeholder
+            const imgElement = document.createElement("img");
+            imgElement.src = product.images[1];//colocamos la segunda imagen
+            imgElement.className = "card-img-top";
+            imgElement.alt = product.title;
+            imgElement.style.width = "100%";
+            imgElement.style.height = "100%";
+            imgElement.referrerPolicy = "no-referrer"; //Esta opción indica que no se debe enviar ningún dato de Referer con la solicitud
+
+             card.insertBefore(imgElement, card.firstChild);
+            
+          }
+          const desc = card.querySelector(".card-text");
+          if (desc) {
+            desc.innerHTML = `<strong>${product.title}</strong><br>${product.description}`;
+          }
+          const price = card.querySelector(".text-body-secondary");
+          if (price) {
+            price.textContent = `$${product.price}`;
+          }
+        }
+      })
+      .catch(error => {
+        console.error("Error al cargar los productos:", error);
+      });
+  });
+});
